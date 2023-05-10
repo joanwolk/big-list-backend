fa8a133a69346dacaa82afb840f91a86c9423abe
Nicky Dover <ndover@bonobos.com>
2023-05-10 18:09:20 -0400

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :items, only: [:index]
+
+
 end

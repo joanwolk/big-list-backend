b91bf0a8c3bed1c4ad68d8957daf9771d3bc22ec
Nicky Dover <ndover@bonobos.com>
2023-05-11 20:55:11 -0400

    reject empty params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@
 
 class ItemsController < ApplicationController
   def index
-    permitted_params = params.permit(:year, :item_number, :item_text, :points, :rating)
+    permitted_params = params.permit(:year, :item_number, :item_text, :points, :rating).reject{|_, v| v.blank?}
     render json: Item.filter_by_params(permitted_params)
   end
 end

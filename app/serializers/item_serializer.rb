3e17c66d3e85ebd88e4522992c8af90929e2ea4a
Nicky Dover <ndover@bonobos.com>
2023-05-12 16:28:31 -0400

    remove errata till added

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,3 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :year, :item_number, :item_number_addendum, :original_item, :spellchecked_item, :special_event, :points, :point_value, :rating, :comment, :errata
+  attributes :id, :year, :item_number, :item_number_addendum, :original_item, :spellchecked_item, :special_event, :points, :point_value, :rating, :comment
 end
